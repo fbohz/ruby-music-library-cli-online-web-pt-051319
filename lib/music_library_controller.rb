69dc4f14abc42c873ada7ddd196f71cbc3951bc4
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-14 23:15:11 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,7 +7,7 @@ class MusicLibraryController
   
   def call 
     puts "Welcome to your music library!"
-    input = gets.chomp
+    
     until input == "exit"
       
       puts "To list all of your songs, enter 'list songs'."
